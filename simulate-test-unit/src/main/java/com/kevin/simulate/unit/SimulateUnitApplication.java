3bc7335d24753b4aca2e70726787f04ee5d7447f
kevin <yundan_wy@126.com>
2022-07-27 09:11:37 +0800

    初始化项目

---
+++ simulate-test-unit/src/main/java/com/kevin/simulate/unit/SimulateUnitApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.simulate.unit;
+
+import com.kevin.simulate.spring.boot.annotation.SpringBootApplication;
+
+/**
+ * @author kevin
+ */
+@SpringBootApplication
+public class SimulateUnitApplication {
+
+
+
+}
